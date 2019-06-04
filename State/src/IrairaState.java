1729527fc798c88d2030ea2399f5d4d6ae12b162
miyazawa-moya <49540461+miyazawa-moya@users.noreply.github.com>
2019-06-04 13:38:26 +0900

    java_State_test_次回課題

---
+++ State/src/IrairaState.java
@@ -0,0 +1,17 @@
+class IrairaState extends DogState {
+    private static IrairaState s = new IrairaState();
+    private IrairaState(){}
+
+    public static DogState getInstance(){
+        return s;
+    }
+
+    public void tukareta(Dog moto){}
+    public void tabeta(Dog moto){
+        moto.changeState(FutsuuState.getInstance());
+    }
+
+    public String toString(){
+        return "いらいら状態";
+    }
+}
